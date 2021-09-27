28f11d7dc1ea456dab42eaae8fb7fccaba21b6f5
周公来 <zhougonglai@nn.com>
2021-09-27 09:54:36 +0800

    code start

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,11 @@
 import vercel from '@sveltejs/adapter-vercel';
+import preprocess from 'svelte-preprocess';
+import { optimizeImports } from 'carbon-preprocess-svelte';
 
 export default {
+  preprocess: [preprocess({ postcss: true, pug: true }), optimizeImports()],
   kit: {
     adapter: vercel(),
-    target: '#svelte',
-  },
+    target: '#svelte'
+  }
 };
